afe7f437c20e57092db8a5a2515f1db80782d209
pawelwilga <pawel@wilga..net.pl>
2025-04-29 19:07:37 +0200

    Zmodyfikowane widoki i kontrolery

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,12 @@
 const { MENU_LINKS } = require("../constants/navigation");
 
 exports.getHomeView = (request, response) => {
+  const cartCount = Cart.getProductsQuantity();
   response.render("home.ejs", {
     headTitle: "Shop - Home",
     path: "/",
     activeLinkPath: "/",
     menuLinks: MENU_LINKS,
+    cartCount,
   });
 };
--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,12 +1,15 @@
+const Cart = require('../models/Cart');
 const { LOGOUT_LINKS } = require("../constants/navigation");
 const logger = require("../utils/logger");
 
 exports.getLogoutView = (request, response) => {
+  const cartCount = Cart.getProductsQuantity();
   response.render("logout.ejs", {
     headTitle: "Shop - Logout",
     path: "/logout",
     activeLinkPath: "/logout",
     menuLinks: LOGOUT_LINKS,
+    cartCount,
   });
 };
 
